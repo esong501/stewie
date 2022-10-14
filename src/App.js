2cad926dc19f5e64eb6afe6b275ffa72ae472900
esong501 <y0501esong@gmail.com>
2022-10-13 21:31:56 -0400

    inital tests/messing around with js and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// Asynchronous functions are required for API calls
+// but, it's not entirely clear how to obtain the result outside
+// of the log.
+
+// class api_test extends React.Component {
+//   async function () {
+//     const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//     const data = await response.json() //extract JSON from the http response
+//   }
+// }
 
 function App() {
   return (
@@ -17,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"} className="ditto" alt="ditto" />
       </header>
     </div>
   );
