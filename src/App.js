6d4d7b5e00b6601579e617c3a572ec311759c426
Jeffrey Hsu <jeffreyhsu01@gmail.com>
2022-10-17 12:37:14 -0400

    Firebase connected to frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { db } from './firebase';
+import { useEffect, useState } from 'react';
+import { onValue, set, ref } from 'firebase/database';
 
 function App() {
+  // Testing, remove later
+  const [recipe, setRecipe] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //read
+  useEffect(() => {
+    onValue(ref(db), snapshot => {
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).map((recipe) => {
+          setRecipes((oldArray)  => [...oldArray, recipe]);
+        });
+      }
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {recipes.map((recipe) => ( // Map each recipe and then print out ingredients
+            <h1>Ingredients: {JSON.stringify(recipe.name.ingredients)}</h1>
+          ))}
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BroswerRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BroswerRouter>
+    <BrowserRouter>
       <App />
-    </BroswerRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
