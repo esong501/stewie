897790d34179ea40b85b3e89eed8208de4d40c5a
esong501 <y0501esong@gmail.com>
2022-11-07 07:00:52 -0500

    linked pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [recipe, setRecipe] = useState("");
   const [recipes, setRecipes] = useState([]);
   
-  setRecipes((oldArray)  => [...oldArray, recipe]); // spread out the old array, and add the new recipe
+  // setRecipes((oldArray)  => [...oldArray, recipe]); // spread out the old array, and add the new recipe
 
   useEffect(() => {
     onValue(ref(db), snapshot => {
@@ -32,38 +32,54 @@ function App() {
     })
   }, []);
 
+  // const navigate = useNavigate();
+
   return (  
-    <div>
-      {/* <Header /> */}
-      {/* <Recipe name="Baked Chicken" tbn={bchick} desc="6 bone-in chicken breast halves, or 6 chicken thighs and wings, skin-on
-        1/2 teaspoon coarse salt
-        1/2 teaspoon Mrs. Dash seasoning
-        1/4 teaspoon freshly ground black pepper
-        "/> */}
-      {/* <Recipe /> */}
-      {/* <ExpandRecipe /> */}
-      {/* <Recipe />
-      <Recipe />
-      <Recipe /> */}
-      {/* <rcolumn /> */}
-    
-      {/* <h1>Instructions: {recipe.instructions[0]}</h1> */}
+    <Router>
       <div>
         {console.log(recipes)}
-        {recipes.map((recipe) => ( // Map each recipe and then print out ingredients
-          // <h1>Instructions: {recipe.instructions[0]}</h1>
-          // <Recipe tbn={recipe.image} name={recipe.label} desc={recipe.instructions[1]}></Recipe> // this is an issue where the array has to be init
-          // <Recipe recipe={recipe}></Recipe> 
-          <ExpandRecipe recipe={recipe}></ExpandRecipe>
-          ))}
-      </div>
+          {recipes.map((recipe) => ( // Map each recipe and then print out ingredients
+            // <h1>Instructions: {recipe.instructions[0]}</h1>
+            // <Recipe tbn={recipe.image} name={recipe.label} desc={recipe.instructions[1]}></Recipe> // this is an issue where the array has to be init
+            // <Recipe recipe={recipe}></Recipe> 
+            // <ExpandRecipe recipe={recipe}></ExpandRecipe>
+            <Routes>
+              <Route path="/" element={<Recipe />}/>
+              <Route path="/recipedetails" element={<ExpandRecipe recipe={recipe}/>} />
+            </Routes>
+            ))}
         {/* <Routes>
-          <Route path="/recipe" element={<Recipe />}/>
-          <Route path="/recipedetail" element={<ExpandRecipe/>} />
+          <Route path="/" element={<Recipe />}/>
+          <Route path="/recipedetails" element={<ExpandRecipe/>} />
         </Routes> */}
-      <Footer />
-    
-    </div>
+        {/* <Header /> */}
+        {/* <Recipe name="Baked Chicken" tbn={bchick} desc="6 bone-in chicken breast halves, or 6 chicken thighs and wings, skin-on
+          1/2 teaspoon coarse salt
+          1/2 teaspoon Mrs. Dash seasoning
+          1/4 teaspoon freshly ground black pepper
+          "/> */}
+        {/* <Recipe /> */}
+        {/* <ExpandRecipe /> */}
+        {/* <Recipe />
+        <Recipe />
+        <Recipe /> */}
+        {/* <rcolumn /> */}
+      
+        {/* <h1>Instructions: {recipe.instructions[0]}</h1> */}
+        {/* <div>
+          {console.log(recipes)}
+          {recipes.map((recipe) => ( // Map each recipe and then print out ingredients
+            // <h1>Instructions: {recipe.instructions[0]}</h1>
+            // <Recipe tbn={recipe.image} name={recipe.label} desc={recipe.instructions[1]}></Recipe> // this is an issue where the array has to be init
+            // <Recipe recipe={recipe}></Recipe> 
+            <ExpandRecipe recipe={recipe}></ExpandRecipe>
+            ))}
+        </div> */}
+          
+        {/* <Footer /> */}
+      
+      </div>
+    </Router>
   );
 
   
--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    {/* <Router>
       <App/>
-    </Router>
+    </Router> */}
+    <App/>
      
-  </React.StrictMode>
+  </React.StrictMode>,
 );
