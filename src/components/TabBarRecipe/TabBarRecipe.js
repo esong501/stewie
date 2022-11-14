c5c08b8afa71698f0e059907b75803456ef0f105
Jeffrey Hsu <jeffreyhsu01@gmail.com>
2022-11-13 20:35:05 -0500

    Added in inner shadow and styled scrollbar

--- src/components/TabBarRecipe/TabBarRecipe.js
+++ src/components/TabBarRecipe/TabBarRecipe.js
@@ -15,7 +15,7 @@ function TabBarRecipe(props) {
         <div class="Tabs">
              <div class="TabBar">
                 <Tabs value = {tab} aria-label="" TabIndicatorProps={{style: {background:'#4848C1'}}} centered >
-                    <Tab value = "preview" icon={<img src={Preview} width="25" height="25" alt="" />} className='tabIcon' label ="Preview" onClick={() => setTab("preview")}/>
+                    <Tab value = "preview" icon={<img src={Preview} width="25" height="25" alt=""/>} className='tabIcon' label ="Preview" onClick={() => setTab("preview")}/>
                     <Tab value = "instruc" icon = {<img src={Cook} width="25" height="25" alt="" />} className='tabIcon' label="Steps" onClick={() => setTab("instruc")}/>
                     <Tab value = "notes" icon = {<img src={Notes} width="25" height="25" alt="" />} className='tabIcon' label="Notes" onClick={() => setTab("notes")}/>
                 </Tabs>
