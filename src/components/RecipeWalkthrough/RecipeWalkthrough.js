d23e38951c0548f684c4abc77b79e3f8c0d26f24
esong501 <y0501esong@gmail.com>
2022-11-21 01:32:02 -0500

    adding modals

--- src/components/RecipeWalkthrough/RecipeWalkthrough.js
+++ src/components/RecipeWalkthrough/RecipeWalkthrough.js
@@ -2,53 +2,17 @@ import { Route, Routes, useState , useEffect, useContext } from "react";
 import CompleteRecipe from "../CompleteRecipe/CompleteRecipe.js";
 import './RecipeWalkthrough.scss'
 import PropTypes from 'prop-types';
-import { FormGroup, FormControlLabel, Checkbox, LinearProgress, Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
-import {ProgressContext} from './../ExpandRecipe/ExpandRecipe.js'
-
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// const theme = createTheme({
-//     palette: {
-//       primary: "#3B9F2B",
-//     },
-//   });
-
-function LinearProgressWithLabel(props) {
-    return (
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box sx={{ width: '100%', mr: 1 }}>
-          <LinearProgress variant="determinate" {...props} />
-        </Box>
-        <Box sx={{ minWidth: 35 }}>
-          <Typography variant="body2" color="text.secondary">{`${Math.round(
-            props.value,
-          )}%`}</Typography>
-        </Box>
-      </Box>
-    );
-  }
-  
-LinearProgressWithLabel.propTypes = {
-/**
- * The value of the progress indicator for the determinate and buffer variants.
- * Value between 0 and 100.
- */
-value: PropTypes.number.isRequired,
-};
-
-
+import { FormGroup, FormControlLabel, Checkbox, LinearProgress, Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal } from '@mui/material';
+import { ProgressContext } from './../ExpandRecipe/ExpandRecipe.js'
+import { Close } from '@mui/icons-material/';
 
 function RecipeWalkthrough(props) {
     const [finish, setFinish] = useState(false);
-    // const [checked, setChecked] = useState(Array(props.steps.length).fill(false));
     const [checked, setChecked] = useState([]);
     const { progress, setProgress } = useContext(ProgressContext);
-    // console.log(useContext(ProgressContext))
-
-    const handleClick = (index) => {
-        checked[index] = !checked[index];
-        console.log(checked)
-    };
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     const handleCheck = (event) => {
         let updatedList = [...checked];
@@ -59,35 +23,40 @@ function RecipeWalkthrough(props) {
         }
         setChecked(updatedList);
         setProgress((updatedList.length/props.steps.length)*100);
+        if (updatedList.length === props.steps.length){
+            handleOpen();
+        }
     };
-
-    // const decClick = () => {
-    //     setIndex(index - 1);
-    // }
-
-    // const recipeWalk = (
-    //     <div className="steps">
-    //         <h4>{props.steps[index]}</h4>
-    //         {index > 0 ? <Button variant="contained" className="prevButton" onClick={decClick}>Previous Steps</Button> : null}
-    //         {index < (props.steps.length - 1) ? <Button variant="contained" className="nextButton" onClick={handleClick}>Next Steps</Button> : null}
-    //         {index === (props.steps.length - 1) ? <Button variant="contained" className="nextButton" onClick={() => setFinish(!finish)}>Finish Recipe!</Button> : null}
-    //     </div> 
-    // );
 
     const recipeWalk = (
         <div>
             <div className="WalkthruScrollElems">
-                {/* {console.log(props.steps)}
-                {console.log(checked)} */}
-                
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box className='finishModal'>
+                        <div>
+                            <Typography id="modal-modal-title" className='modalTitle'>
+                                Finished Cooking?
+                            </Typography>
+                        </div>
+                        <Button variant="text" class='exitModal' startIcon={<Close />} onClick={handleClose}/>
+                        <div className="exitButtons">
+                            <CompleteRecipe/>
+                            <Button variant="text" class='noButton' onClick={handleClose}>No, Keep Cooking</Button>
+                        </div>
+                    </Box>
+                </Modal>
                 <tbody>
                     {props.steps.map((steps, index) =>
                         <tr key={index}>
                             <td>
                                 <h4>
+                                
                                     <FormGroup className="steps">
-                                        {/* {console.log(steps)}
-                                        {console.log(index)}  */}
                                         <FormControlLabel sx={{fontFamily: 'monarcha'}} control={<Checkbox size="large" style ={{color: "#3B9F2B",}} onChange={handleCheck}/>} label={<Typography className="steps">{steps}</Typography>}/>
                                     </FormGroup>
                                 </h4>
