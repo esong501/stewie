62f7ea5400621e4c9797a2a80f712c74fe54aab9
Jeffrey Hsu <jeffreyhsu01@gmail.com>
2022-11-20 21:02:18 -0500

    Minor css changes to Dictionary

--- src/components/Dictionary/Dictionary.js
+++ src/components/Dictionary/Dictionary.js
@@ -5,7 +5,7 @@ function Dictionary(props) {
     return (
         <div class="Dict">
             {/* <FormControlLabel sx={{fontFamily: 'monarcha'}} /> */}
-            <h4><span class="DictWord">{props.word}:</span> pat with paper towel</h4>
+            <p><span class="DictWord">{props.word}:</span> pat with paper towel</p>
         </div>
     );
 }
