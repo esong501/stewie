ae712138397d2d9caecff2189e4b29ce14b29008
MuhammedAbdalla <muhabdalla718@gmail.com>
2022-12-02 18:57:27 -0500

    working on rendering issue; grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './img/logo.svg';
 // import './App.css';
 import { db } from './services/Firebase.js'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo} from 'react';
 import { onValue, ref, get, child, getDatabase } from 'firebase/database';
 import React from 'react';
 import * as ReactDOM from 'react-dom';
@@ -11,52 +11,87 @@ import RecipeWalkthrough from './components/RecipeWalkthrough/RecipeWalkthrough.
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
+import { useRef } from 'react';
 
-function App() {
-  // Testing, remove later
-  const [recipes, setRecipes] = useState([]);
-  const dbRef = ref(getDatabase());
-  // setRecipes((oldArray)  => [...oldArray, recipe]); // spread out the old array, and add the new recipe
+const RecipeGrid = ({recipes}) => (
+  <>
+    {recipes && recipes.map((singleRecipe) => (
+        <Recipe key={singleRecipe.label} recipe={singleRecipe}/>
+    ))}
+</>
+);
 
-  async function getRecipes() {
-    await get(child(dbRef,'recipes_5'))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        setRecipes(snapshot.val().recipes_5.value); // Add each recipe to the recipes array
-      }
-    });
-  }
+export default function App() {
+    // Testing, remove later
+    const [recipes, setRecipes] = useState([]);
+    const componentIsMounted = useRef(true);
+    // setRecipes((oldArray)  => [...oldArray, recipe]); // spread out the old array, and add the new recipe
 
-  useEffect(() => {
-    //   onValue(ref(db), snapshot => {
-    //     const data = snapshot.val().recipes_5.value; // Data is the recipe
-    //     console.log(data);
-    //     if (data !== null) {
-    //       setRecipes(data); // Add each recipe to the recipes array
-    //   }
-    // })
-    getRecipes();
-  }, []);
+    // async function getRecipes() {
+    //   await get(child(dbRef,'recipes_5'))
+    //   .then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //       setRecipes(snapshot.val().recipes_5.value); // Add each recipe to the recipes array
+    //     }
+    //   });
+    // }
 
-  return (
-    <Router>
-      <Header/>
-      <div className='pagehead'>Recipes</div>
-        <div className='maindiv'>
-          <div className='row'>
-            {recipes.map((singleRecipe) => (
-              <Routes>
-                {console.log(singleRecipe)}
-                  <Route path="/" element={<Recipe recipe = {singleRecipe}/>}/>
-                  <Route path={"/recipedetails/"+singleRecipe.recipe.label} element={<ExpandRecipe recipe={singleRecipe}/>}/>
-                </Routes>
-            ))}
+    // useEffect(() => {
+    //     onValue(ref(db), snapshot => {
+    //       const data = snapshot.val().recipes_5.value; // Data is the recipe
+    //       console.log(data);
+    //       if (data !== null) {
+    //         setRecipes(data); // Add each recipe to the recipes array
+    //     }
+    //   })
+    // }, []);
+    useEffect(() => {
+        // each useEffect can return a cleanup function
+        return () => {
+            componentIsMounted.current = false;
+        };
+    }, []); // no extra deps => the cleanup function run this on component unmount
+
+    useEffect(() => {
+        async function getData() {
+            const dbRef = ref(db, "recipes_5");
+            await get(dbRef).then((snapshot) => {
+                if (snapshot.exists() && componentIsMounted.current) {
+                    console.log(snapshot.val().value);
+                    setRecipes(snapshot.val().value); // Add each recipe to the recipes array
+                }
+            })
+        }
+
+        getData();
+    }, []);
+
+    
+    
+
+    return (
+      <>{recipes && (<Router>
+            <Header />
+            <div className = 'pagehead' > Recipes </div>
+            <div className = 'maindiv' >
+            <div className = 'row' >
+              <Routes >
+                  {recipes && recipes.map((singleRecipe) => {
+                      console.log("/recipedetails/" + encodeURIComponent(singleRecipe.label));
+                      return (
+                          <Route path = { "/recipedetails/" + encodeURIComponent(singleRecipe.label) }
+                              element = {<ExpandRecipe key = { singleRecipe.label } recipe = { singleRecipe }/> }
+                          />
+                      )
+                  })}
+                  <Route path="/" element={<RecipeGrid recipes={recipes}/>}/>
+              </Routes> 
+          </div>  
           </div> 
-        </div>
-    </Router>
-  );
-}
-  
-ReactDOM.render(<App />, document.getElementById('root'));
+        </Router>)
+      }</>)
+    }
+    
 
-export default App;
+
+    ReactDOM.render( < App /> , document.getElementById('root'));
